1e730ffdf8fbada12947fc496cafc1c9a8c59068
rhysw97 <62967056+rhysw97@users.noreply.github.com>
2023-01-17 21:27:58 +0000

    worked on sequencer

---
+++ src/Components/Synth/Sequencer/Row/row.js
@@ -0,0 +1,15 @@
+import React from "react";
+import * as Tone from 'tone'
+
+export default function row(p) {
+    
+
+    for(let i = 0; i < p.steps; i++) {
+
+    }
+    return(
+        <div>
+
+        </div>
+    )
+}
--- src/Components/Synth/Sequencer/sequencer.tsx
+++ src/Components/Synth/Sequencer/sequencer.tsx
@@ -8,6 +8,7 @@ import { SynthContext } from '../../../App';
 export default function Sequencer(props: any) {
     const synth = useContext(SynthContext)
     const sequence = ["C4", ["E4", "D4"], "G4", ["A4", "G4"]]
+    const grid: {rowName: string, steps:any[]}[]= [];
     const playSequence = () => {
         const seq = new Tone.Sequence((time, note) => {
             synth.triggerAttackRelease(note, 0.1, time);
@@ -21,10 +22,27 @@ export default function Sequencer(props: any) {
 
     const createSequencer = (notes:string[]) => {
         notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"]
+        notes.forEach(note => {
+            const row = []
+            for(let i = 0; i < 16; i++) {
+                row.push(
+                    <div className="step">
+                        <div className="isActive"></div>
+                    </div>
+                )
+            }
+            grid.push({
+                rowName: note,
+                steps: row
+            })
+        })
         
     }
     return (
         <div>
+            <div>
+            
+            </div>
             <div onClick={playSequence}>Play Sequence</div>
             <div onClick={stopSequence}>Stop Sequence</div>
         </div>
